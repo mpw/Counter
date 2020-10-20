e7f0f9b11224d98d8e3f2965c975f5165f5de15d
Marcel Weiher <marcel@metaobject.com>
2020-10-20 20:05:48 +0200

    counter working

--- CounterExample/AppDelegate.h
+++ CounterExample/AppDelegate.h
@@ -9,6 +9,7 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
+@property (strong) IBOutlet NSTextField *field;
 
 @end
 
--- CounterExample/AppDelegate.m
+++ CounterExample/AppDelegate.m
@@ -13,15 +13,16 @@
 @end
 
 @implementation AppDelegate
-
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+{
+    int counter;
 }
 
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+-(IBAction)inc:sender
+{
+    counter++;
+    [self.field setIntValue:counter];
 }
+
 
 
 @end
